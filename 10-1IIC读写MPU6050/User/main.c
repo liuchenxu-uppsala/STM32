b0ad8eeacd001dc0adeec2901604fc736a8ded5c
liuchenxu-uppsala <chenxu.l@outlook.com>
2025-10-26 15:29:45 +0100

    MPU6050封装IIC读写操作

--- 10-1IIC读写MPU6050/User/main.c
+++ 10-1IIC读写MPU6050/User/main.c
@@ -1,15 +1,14 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
-#include "SimpleIIC.h"
+#include "MPU6050.h"
 
 int main(void)
 {
 	/*模块初始化*/
 	OLED_Init();		//OLED初始化
-	Init_IIC();
-	IIC_Start();
-	IIC_SendByte(0xD0);
-	uint8_t ack = IIC_RecvACk();
-	OLED_ShowNum(1,1,ack,2);
+	Init_MPU6050();
+	Write_Data(0x19,0xAA);
+	uint8_t data = Read_Data(0x19);
+	OLED_ShowHexNum(1, 1, data, 2);
 }
